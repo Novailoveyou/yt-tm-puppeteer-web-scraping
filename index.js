7650cb79b029b7f0d4b8df7a53118676ed062199
novailoveyou <novailoveyoux@gmail.com>
2022-11-13 21:55:45 +0300

    get all courses

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer')
 async function run() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://www.google.com')
+  await page.goto('https://www.traversymedia.com')
 
   // await page.screenshot({ path: 'export/example.png', fullPage: true })
   // await page.pdf({ path: 'export/example.pdf', format: 'A4' })
@@ -14,11 +14,20 @@ async function run() {
 
   // const text = await page.evaluate(() => document.body.innerText)
 
-  const links = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('a'), e => e.href)
+  // const links = await page.evaluate(() =>
+  //   Array.from(document.querySelectorAll('a'), e => e.href)
+  // )
+
+  const courses = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('#courses .card'), e => ({
+      title: e.querySelector('.card-body h3').innerText,
+      level: e.querySelector('.card-body .level').innerText,
+      url: e.querySelector('.card-footer a').href,
+      promo: e.querySelector('.card-footer .promo-code .promo').innerText
+    }))
   )
 
-  console.log(links)
+  console.log(courses)
 
   await browser.close()
 }
