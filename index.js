d2fcf0a35a4d9573be6aa073eb765aaf570fa9d8
novailoveyou <novailoveyoux@gmail.com>
2022-11-13 21:28:34 +0300

    updated gitignore to include export folder & updated index.js to export onto export folder

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ async function run() {
   const page = await browser.newPage()
   await page.goto('https://www.google.com')
 
-  // await page.screenshot({ path: 'example.png', fullPage: true })
-  await page.pdf({ path: 'example.pdf', format: 'A4' })
+  await page.screenshot({ path: 'export/example.png', fullPage: true })
+  await page.pdf({ path: 'export/example.pdf', format: 'A4' })
 
   await browser.close()
 }
