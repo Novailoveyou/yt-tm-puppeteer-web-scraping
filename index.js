019ba42e00e7eec2a904e3b3f58625e6717d1c6c
novailoveyou <novailoveyoux@gmail.com>
2022-11-13 21:30:01 +0300

    get full page html

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ async function run() {
   const page = await browser.newPage()
   await page.goto('https://www.google.com')
 
-  await page.screenshot({ path: 'export/example.png', fullPage: true })
-  await page.pdf({ path: 'export/example.pdf', format: 'A4' })
+  // await page.screenshot({ path: 'export/example.png', fullPage: true })
+  // await page.pdf({ path: 'export/example.pdf', format: 'A4' })
+
+  const html = await page.content()
+  console.log(html)
 
   await browser.close()
 }
